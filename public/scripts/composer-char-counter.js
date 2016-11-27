8f231c573de0a2cc5c4f860a8f5704285e8ce5f3
Dom Golac <domgolac@gmail.com>
2016-11-27 00:17:18 +0000

    Added composer-char-counter.js

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).on("ready", function() {
+  $(".new-tweet").on("keyup", "textarea", function(event) {
+    var counter = 140;
+    var length = $(this).val().length;
+    counter -= length;
+    $(this).closest(".new-tweet").find(".counter")[0].innerText = Number(counter);
+    if (length >= 140) {
+      $(".counter").addClass("red-text");
+    };
+    if ($(".counter").hasClass("red-text") && length < 140) {
+      $(".counter").removeClass("red-text");
+    };
+  });
+});
